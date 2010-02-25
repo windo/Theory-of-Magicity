2c2c5575f9891194e0d7d0da72148ea821c399cb
Siim Põder <siim@p6drad-teel.net>
2010-02-25 02:59:20 +0200

    graphics.py: use GL_NEAREST instead of GL_LINEAR (removes odd alpha border)

--- graphics.py
+++ graphics.py
@@ -48,8 +48,8 @@ class glImg:
           tex = glGenTextures(1)
           self.__texture = tex
           glBindTexture(GL_TEXTURE_2D, tex)
-          glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-          glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+          glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+          glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
           glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, texdata)
     
           # create genlist
